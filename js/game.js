b50225beafdb106d478b0ff953d71d8aba3bdb57
Gabriel-JN <gabriel.jorge19999@gmail.com>
2022-11-08 22:43:55 -0300

    creating the function to sort the cards

---
+++ js/game.js
@@ -0,0 +1,45 @@
+const grid = document.querySelector('.grid');
+
+const characters = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'summer',
+    'meeseeks',
+    'scroopy',
+];
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+const createCard = () => {
+
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+
+    card.appendChild(front);
+    card.appendChild(back);
+    grid.appendChild(card);
+
+    return card;
+}
+
+const loadGame = () => {
+
+    characters.forEach((character) => {
+
+        const card = createCard();
+        grid.appendChild(card);
+
+    });
+}
+
+loadGame();
